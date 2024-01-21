3bfb07e73c92d831b99cbca03ef330de95e274e3
Subrat kumar jena <subratj611@gmail.com>
2024-01-21 18:50:16 +0530

    fix

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -9,12 +9,11 @@ export default function DefaultTemplate({ children }) {
 
   return (
     <main className="h-screen flex">
-      <SideNav isNavShow={isNavShow} setIsNavShow={setIsNavShow} />
+      <SideNav />
       <section className="h-full w-full pt-6 px-5">
         <Button
           variant="ghost"
           className="px-2 bg-transparent focus:bg-gray-100 rounded-full md:hidden"
-          onClick={() => setIsNavShow((prev) => !prev)}
         >
           <HamburgerMenuIcon className="text-gray-600 h-5 w-5" />
         </Button>
